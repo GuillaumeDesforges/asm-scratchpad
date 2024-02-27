dd6a4ea30d7839fc603234b8e3126b60c9a97200
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2024-02-27 07:46:38 +0100

    test: improve fibonacci

--- test.asm
+++ test.asm
@@ -1,37 +1,27 @@
 section .data
-; -----
-; Define constants
-EXIT_SUCCESS equ 0 ; successful operation
-SYS_exit equ 60 ; call code for terminate
-; -----
-; Variables for addition
-dq_n dq 0
-dq_i dq 0
-dq_x dq 0
-dq_y dq 0
-; ************************************************************
-; Code Section
+
+n dq 3                ; n
+
+x dq 0                ; fib(n)
+y dq 0                ; fib(n+1)
+
 section .text
 global _start
 _start:
-  mov qword [dq_n], 5
-  mov qword [dq_i], 1
-  mov qword [dq_x], 0
-  mov qword [dq_y], 1
-  jmp loopStep
+  mov rcx, [n]
+  mov qword [x], 0
+  mov qword [y], 1
+  cmp rcx, 1
+  jle exit
 loopStep:
-  inc qword [dq_i]
-  mov rax, qword [dq_y]
-  mov rdx, qword [dq_y]
-  add rax, qword [dq_x]
-  mov qword [dq_x], rdx
-  mov qword [dq_y], rax
-  mov rax, qword [dq_i]
-  cmp rax, qword [dq_n]
-  jle loopStep
-; ************************************************************
-; Done, terminate program.
-last:
- mov rax, SYS_exit ; Call code for exit
- mov rdi, EXIT_SUCCESS ; Exit program with success
- syscall
+  mov rax, qword [y]
+  add rax, qword [x]  ; rax <- fib(n+1) + fib(n)
+  mov rdx, qword [y]
+  mov qword [x], rdx
+  mov qword [y], rax  ; fib(n), fib(n+1) <- fib(n+1), rax
+  loop loopStep
+exit:
+  ; result is in x
+  mov rax, 60
+  mov rdi, 0
+  syscall                ; Exit program with success
