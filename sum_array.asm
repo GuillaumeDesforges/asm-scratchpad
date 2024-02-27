2aa237b6c107be36be99ebc8a547317fca45d510
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2024-02-27 08:05:13 +0100

    sum_array: use iterator and loop

--- sum_array.asm
+++ sum_array.asm
@@ -2,6 +2,7 @@ section .data
 
 n_values dq 5
 values dq 1,10,100,1000,10000
+result dq 0
 
 section .text
 
@@ -9,11 +10,14 @@ global _start
 _start:
 
 ; sum all values
-mov rax, 0
-mov rcx, [n_values]
+mov rax, 0                     ; result
+mov rcx, [n_values]            ; counter (decremental)
+mov rdx, values                ; iterator
 sumStep:
-add rax, [values+(rcx-1)*8]
+add rax, qword [rdx]           ; add element at iterator
+add rdx, 8                     ; next item
 loop sumStep
+mov [result], qword rax
 
 ; exit with success
 exit:
