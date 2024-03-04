ee3461b5b33e49bf8c946f1bb896b4543a4bebfb
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2024-03-04 18:07:49 +0900

    map: real functional map yay!

--- map.asm
+++ map.asm
@@ -6,54 +6,75 @@ result        dq 0,0,0,0
 
 section .text
 
-; int_double :: uint64* -> uint64* -> void
+; int_double(
+;   x: uint64
+; ): uint64
 global int_double
 int_double:
   ; prologue
   push rbp
   mov rbp, rsp
   ; body
-  mov rax, qword [rdi]
+  mov rax, rdi
   mov rdx, 2
   mul rdx
-  mov qword [rsi], rax
   ; epilogue
   mov rsp, rbp
   pop rbp
   ret
 
-global _start
-
-_start:
-
-  mov rcx, [len]
+; qword_map(
+;   f_address: (qword -> qword)*
+;   source_array_address: array<qword>*
+;   destination_array_address: array<qword>*
+;   length: uint64
+; ): void
+global qword_map
+qword_map:
+  ; prologue
+  push rbp
+  mov rbp, rsp
+  push rdi ; f_address
+  push rsi ; source_array_address
+  push rdx ; destination_array_address
+  ; push rcx ; length ; not needed
+  ; body
+  ; mov rcx, [rbp+32] ; not needed
   mov rsi, 0
 iterLoop:
-  ; prepare call to int_double on item at &array+$rsi*8
-  ; rdi <- &array + $rsi * 8
-  mov rdi, array
-  mov rax, rsi
-  mov rdx, 8
-  mul rdx
-  add rdi, rax
-  ; rsi <- &result + $rsi * 8
-  mov rax, rsi
-  push rsi ; save iterator here since rsi is part of the call convention (second uint64 arg)
-  mov rsi, result
-  mov rdx, 8
-  mul rdx
-  add rsi, rax
-  ; do a little trick: store which function to use in rax
-  ; so that technically this could call any function on this item
-  ; like map should
-  mov rax, int_double
-  call rax
-  pop rsi ; recover iterator
-  inc rsi ; iterate
+  ; prepare call to f on item at source_array_address+rsi*8
+  ; rdi <- *(&source_array_address + rsi * 8)
+  mov rdi, qword [rbp-16]
+  mov rdi, qword [rdi+(rsi*8)]
+  ; rax <- f(rdi)
+  push rsi ; rsi might disappear, save it
+  push rcx ; rcx might disappear, save it
+  call qword [rbp-8]
+  pop rcx ; recover rcx
+  pop rsi ; recover rsi
+  ; *(&destination_array_address + rsi * 8) <- rax
+  mov rdi, qword [rbp-24]
+  mov qword [rdi+(rsi*8)], rax
+  ; iterate
+  inc rsi
   loop iterLoop
+  ; epilogue
+  mov rsp, rbp
+  pop rbp
+  ret
 
+; basically
+; map(&int_double, &array, &result, len)
+global _start
+_start:
+  mov rdi, int_double
+  mov rsi, array
+  mov rdx, result
+  mov rcx, qword [len]
+  call qword_map
 exit:
+  ; exit program with success
   mov rax, 60
   mov rdi, 0
-  syscall                ; Exit program with success
+  syscall
 
